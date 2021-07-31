4941f7ba76e63c150a9fb0bb6434564920984cd4
nikhil076 <nikhil.arvind7@gmail.com>
2021-07-31 16:18:14 +0530

    Refactored to add multiple addressbook to the system

--- lib/src/main/java/Workshop_AddressBook/AddressBookMain.java
+++ lib/src/main/java/Workshop_AddressBook/AddressBookMain.java
@@ -5,17 +5,36 @@ import java.util.Scanner;
 public class AddressBookMain {
 
 	public static void main(String[] args) {
+		boolean result = true;
 		Scanner input = new Scanner(System.in);
 		System.out.println("Welcome to addressbook program");
-		AddressBook addressBook = new AddressBook();
-		addressBook.addContact();
-		
-		System.out.println("Enter your firstname to Edit your details..");
-        String firstname = input.next();
-        addressBook.editContact(firstname);
-        
-        System.out.println("Enter your Firstname to delete your details :");
-        String firstName = input.next();
-        addressBook.deleteContact(firstName);
-	}
+		AddressBook_Interface addressBook = new AddressBook();
+		/**
+         * switch case for each cases to be performed
+         */
+        while(result) {
+            System.out.println("Enter option to make changes : \n[press 1 to add new Contact" +
+                    "\n press 2 to Edit existing contacts" + "\n press 3 to delete a contact detail " + "\n press 4 to search person by city" +
+                    "\n press 5 to search person by state" + "\n press 6 to view person by city" +
+                    "\n press 7 to view person by state" + "\n press 8 to count persons by city or state" + "\n press 9 to display contacts" + "\n press 0 to quit ]");
+            int option = input.nextInt();
+            switch (option) {
+                case 1 :
+                    addressBook.addContact();
+                    break;
+                case 2 :
+                    System.out.println("Enter your firstname to Edit your details..");
+                    String firstname = input.next();
+                    addressBook.editContact(firstname);
+                    break;
+                case 3 :
+                    System.out.println("Enter your Firstname to delete your details :");
+                    String firstName = input.next();
+                    addressBook.deleteContact(firstName);
+                    break;
+                default:
+                    result = false;
+            }
+        }
+    }
 }
