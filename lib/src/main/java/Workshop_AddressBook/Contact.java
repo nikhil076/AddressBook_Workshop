e66a236ed37dc69463fe5a2e1fc2743a05b61b27
nikhil076 <nikhil.arvind7@gmail.com>
2021-07-31 14:54:00 +0530

    Added all aattributes for contact details in addressbook

---
+++ lib/src/main/java/Workshop_AddressBook/Contact.java
@@ -0,0 +1,91 @@
+package Workshop_AddressBook;
+
+public class Contact {
+
+	public String firstname;
+	public String lastname;
+	public String address;
+	public String city;
+	public String state;
+	public String phoneNumber;
+	public int zipcode;
+
+	public Contact(String firstname, String lastname, String address, String city, String state, String phoneNumber,
+			int zipcode) {
+		super();
+		this.firstname = firstname;
+		this.lastname = lastname;
+		this.address = address;
+		this.city = city;
+		this.state = state;
+		this.phoneNumber = phoneNumber;
+		this.zipcode = zipcode;
+	}
+
+	public Contact() {
+		super();
+	}
+
+	public String getFirstname() {
+		return firstname;
+	}
+
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+
+	public String getLastname() {
+		return lastname;
+	}
+
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	public int getZipcode() {
+		return zipcode;
+	}
+
+	public void setZipcode(int zipcode) {
+		this.zipcode = zipcode;
+	}
+
+	@Override
+	public String toString() {
+		return "Contact [firstname=" + firstname + ", lastname=" + lastname + ", address=" + address + ", city=" + city
+				+ ", state=" + state + ", phoneNumber=" + phoneNumber + ", zipcode=" + zipcode + "]";
+	}
+
+}
