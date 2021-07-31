c722cae2d5753a150c72e935ac528a327cfd61e0
nikhil076 <nikhil.arvind7@gmail.com>
2021-07-31 16:02:33 +0530

    Added abstarct method for deleting the contact from addressbook

--- lib/src/main/java/Workshop_AddressBook/AddressBook_Interface.java
+++ lib/src/main/java/Workshop_AddressBook/AddressBook_Interface.java
@@ -3,4 +3,5 @@ package Workshop_AddressBook;
 public interface AddressBook_Interface {
 	public void addContact();
 	public void editContact(String firstname);
+	public void deleteContact(String firstname);
 }
