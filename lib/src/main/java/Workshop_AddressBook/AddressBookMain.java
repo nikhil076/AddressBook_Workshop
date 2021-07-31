2d6d263e18040325e977eb54f88722c3f507f634
nikhil076 <nikhil.arvind7@gmail.com>
2021-07-31 15:23:46 +0530

    Added method calling of editing the contact details in addressbook

--- lib/src/main/java/Workshop_AddressBook/AddressBookMain.java
+++ lib/src/main/java/Workshop_AddressBook/AddressBookMain.java
@@ -1,10 +1,16 @@
 package Workshop_AddressBook;
 
+import java.util.Scanner;
+
 public class AddressBookMain {
 
 	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
 		System.out.println("Welcome to addressbook program");
 		AddressBook addressBook = new AddressBook();
 		addressBook.addContact();
+		System.out.println("Enter your firstname to Edit your details..");
+        String firstname = input.next();
+        addressBook.editContact(firstname);
 	}
 }
