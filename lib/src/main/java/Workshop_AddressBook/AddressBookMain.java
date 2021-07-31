257099f5f53ffbb99132ac030ed232cfe5523fbe
nikhil076 <nikhil.arvind7@gmail.com>
2021-07-31 15:05:29 +0530

    Added the calling method of add contact method

--- lib/src/main/java/Workshop_AddressBook/AddressBookMain.java
+++ lib/src/main/java/Workshop_AddressBook/AddressBookMain.java
@@ -4,6 +4,7 @@ public class AddressBookMain {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to addressbook program");
+		AddressBook addressBook = new AddressBook();
+		addressBook.addContact();
 	}
-
 }
