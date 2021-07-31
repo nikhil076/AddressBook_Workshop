d29410208af7676dbe0c7bfb4112b468a27f9277
nikhil076 <nikhil.arvind7@gmail.com>
2021-07-31 16:32:01 +0530

    Added abstract method to search person by city or state

--- lib/src/main/java/Workshop_AddressBook/AddressBook_Interface.java
+++ lib/src/main/java/Workshop_AddressBook/AddressBook_Interface.java
@@ -4,4 +4,6 @@ public interface AddressBook_Interface {
 	public void addContact();
 	public void editContact(String firstname);
 	public void deleteContact(String firstname);
+	public void searchPersonByCity(String firstname);
+    public void searchPersonByState(String firstname);
 }
