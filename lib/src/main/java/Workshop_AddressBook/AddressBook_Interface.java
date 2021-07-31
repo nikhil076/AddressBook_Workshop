6ed43591b7d8d6c80e6ab8701bf459abea10bedf
nikhil076 <nikhil.arvind7@gmail.com>
2021-07-31 16:50:09 +0530

    Added abstact method for getting count of contactc by city name

--- lib/src/main/java/Workshop_AddressBook/AddressBook_Interface.java
+++ lib/src/main/java/Workshop_AddressBook/AddressBook_Interface.java
@@ -2,10 +2,20 @@ package Workshop_AddressBook;
 
 public interface AddressBook_Interface {
 	public void addContact();
+
 	public void editContact(String firstname);
+
 	public void deleteContact(String firstname);
+
 	public void searchPersonByCity(String firstname);
-    public void searchPersonByState(String firstname);
-    public void viewByCity(String city);
-    public  void viewByState(String state);
+
+	public void searchPersonByState(String firstname);
+
+	public void viewByCity(String city);
+
+	public void viewByState(String state);
+
+	public void CountByCity(String City);
+	
+	public void display();
 }
