e8afc1b28c43e1723445dac7e51c4c5d7dee4a1a
nikhil076 <nikhil.arvind7@gmail.com>
2021-08-01 17:24:08 +0530

    Added test case to check if contact is updated it should sync with the database

--- lib/src/test/java/dbtest/AddressBookDBTest.java
+++ lib/src/test/java/dbtest/AddressBookDBTest.java
@@ -7,6 +7,7 @@ import org.junit.Test;
 
 import addressbookdb.AddressBookServiceController;
 import addressbookdb.AddressBookServiceController.IOService;
+import addressbookdb.Contact;
 
 public class AddressBookDBTest {
 
@@ -22,5 +23,16 @@ public class AddressBookDBTest {
 		addressBookDBController.readContacts(IOService.DB_IO);
 		Integer count = addressBookDBController.contactList.size();
 		assertEquals(Integer.valueOf(4), count);
+	}
+
+	@Test
+	public void test2_givenData_WhenUpdated_ShouldBeInSyncWithDB() {
+		Long expectedPhone = 9988776654l;
+		Long phone = null;
+		addressBookDBController.updatePhoneContact(IOService.DB_IO, "Arvind", "Nikhil", expectedPhone);
+		Contact contact = addressBookDBController.getContact("Arvind", "Nikhil");
+		if (contact != null)
+			phone = contact.getPhoneNo();
+		assertEquals(expectedPhone, phone);
 	}
 }
