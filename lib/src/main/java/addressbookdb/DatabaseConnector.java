7980acc5ca15bdc480d7cf0cd7fdf4479cd0d93b
nikhil076 <nikhil.arvind7@gmail.com>
2021-08-01 16:25:29 +0530

    Added database connectivity implementation

---
+++ lib/src/main/java/addressbookdb/DatabaseConnector.java
@@ -0,0 +1,26 @@
+package addressbookdb;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import AddressBookException.AddressBookException;
+
+public class DatabaseConnector {
+
+	private static Connection con = null;
+
+	// connects to the payroll_service database
+	public static Connection getConnection() throws AddressBookException {
+		String url = "jdbc:mysql://localhost:3306/addressbook_service";
+		String user = "root";
+		String password = "1640";
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			con = DriverManager.getConnection(url, user, password);
+		} catch (SQLException | ClassNotFoundException e) {
+			throw new AddressBookException(e.getMessage());
+		}
+		return con;
+	}
+}
