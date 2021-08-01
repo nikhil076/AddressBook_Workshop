c3b4c1f8db7101fef91a57e911052f0e65b26a22
nikhil076 <nikhil.arvind7@gmail.com>
2021-08-01 14:41:06 +0530

    Added csv file to do operation

--- lib/src/main/java/iostreams/AddressBookMain.java
+++ lib/src/main/java/iostreams/AddressBookMain.java
@@ -1,5 +1,7 @@
 package iostreams;
 
+import com.opencsv.exceptions.CsvException;
+
 public class AddressBookMain {
 
 	public static void main(String[] args) {
@@ -7,6 +9,13 @@ public class AddressBookMain {
 		addressBookFiles.writeDataIntoFile();
 		addressBookFiles.readDataFromFile();
 		addressBookFiles.printData();
-	}
 
+		AddressBookCSV addressBookCSV = new AddressBookCSV(
+				"C:\\Users\\Nikhil Arvind\\Batch307\\Workshop_AddressBook\\lib\\src\\main\\resources\\ContactBook.csv");
+		try {
+			addressBookCSV.readFromCSVFile();
+		} catch (CsvException e) {
+			e.printStackTrace();
+		}
+	}
 }
